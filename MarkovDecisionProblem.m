969e509a947e53c2a8408ebe7e804bf3bf9961d5
Antonio <antonio.amdpaula@gmail.com>
2021-03-03 19:41:15 +0000

    Added auxiliary properties for uniformization

--- MarkovDecisionProblem.m
+++ MarkovDecisionProblem.m
@@ -7,7 +7,10 @@ classdef MarkovDecisionProblem < handle
         nStates = 0;
         actions = [string.empty];
         nActions = 0;
+        exp_actions =[string.empty];
+        nEXPActions = 0;
         transition_matrix = [];
+        exponential_transition_matrix = [];
         initial_state = [];
         reward_matrix = [];
     end
@@ -19,26 +22,49 @@ classdef MarkovDecisionProblem < handle
                 MDP.nStates = MDP.nStates+1;
                 MDP.transition_matrix = padarray(MDP.transition_matrix, [1 0 1],0,'post');
                 MDP.transition_matrix = MDP.transition_matrix(:,:,1:MDP.nStates);
+                
+                MDP.exponential_transition_matrix = padarray(MDP.exponential_transition_matrix, [1 0 1],0,'post');
+                MDP.exponential_transition_matrix = MDP.exponential_transition_matrix(:,:,1:MDP.nStates);
                 state_index = length(MDP.states);
             else
                 disp("WARNING: Tried to add state that already exists")
                 state_index = find(MDP.states == state_name);
             end
         end
-        function action_index = add_action(MDP, action_name)
-            if isempty(find(MDP.actions == action_name))
-                MDP.actions = cat(2, MDP.actions, action_name);
-                MDP.nActions = MDP.nActions+1;
-                MDP.transition_matrix = padarray(MDP.transition_matrix, [0 1 0],0,'post');
-                action_index = length(MDP.actions);
-                
+        function action_index = add_action(MDP, action_name, type)
+            if (type == "imm")
+                if isempty(find(MDP.actions == action_name))
+                    MDP.actions = cat(2, MDP.actions, action_name);
+                    MDP.nActions = MDP.nActions+1;
+                    MDP.transition_matrix = padarray(MDP.transition_matrix, [0 1 0],0,'post');
+                    action_index = length(MDP.actions);
+
+                else
+                    disp("WARNING: Tried to add action that already exists")
+                    action_index = find(MDP.actions == action_name);
+                end
+            elseif (type == "exp")
+                if isempty(find(MDP.exp_actions == action_name))
+                    MDP.exp_actions = cat(2, MDP.exp_actions, action_name);
+                    MDP.nEXPActions = MDP.nEXPActions+1;
+                    MDP.exponential_transition_matrix = padarray(MDP.exponential_transition_matrix, [0 1 0],0,'post');
+                    action_index = length(MDP.exp_actions);
+
+                else
+                    disp("WARNING: Tried to add action that already exists")
+                    action_index = find(MDP.exp_actions == action_name);
+                end
             else
-                disp("WARNING: Tried to add action that already exists")
-                action_index = find(MDP.actions == action_name);
+                error("Action added must be either immediate or exponential")
             end
         end
+        function normalization(MDP)
             
+        end
+                
+                
     end
     
 end
+
 
