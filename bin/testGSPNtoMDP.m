d3658a95ba77dc1fdda4660d66547d0d6a3c39f7
Antonio <antonio.amdpaula@gmail.com>
2021-03-08 12:29:55 +0000

    Tested new functions

--- bin/testGSPNtoMDP.m
+++ bin/testGSPNtoMDP.m
@@ -12,7 +12,7 @@ import MarkovDecisionProcess.MarkovDecisionProcess.*
 % tokens = [1,    0,    0,    0,    0,    0   ];
 % 
 % transitions =    ["t1", "t2", "t3", "t4", "t5"];
-% transition_types=["imm","imm","imm","imm","imm"];
+% transition_types=["imm","i600 ++    872 --mm","imm","imm","imm"];
 % transition_rates=[0,    0,    0,    1,    2   ];
 % 
 % arc_places = ["p1","p1","p1","p2", "p2","p2","p3", "p4", "p5", "p6"];
@@ -26,44 +26,51 @@ import MarkovDecisionProcess.MarkovDecisionProcess.*
 % 
 % MDP = deterministic.ConverttoMDP();
 
-% small_hybrid = GSPNR();
-% places = ["p1", "p2", "p3", "p4"];
-% tokens = [1   , 0   , 0   , 0   ];
-% 
-% transitions =       ["t1", "t2", "t3"];
-% transition_types =  ["imm","imm","exp"];
-% transition_rates =  [0,    0,    1   ];
-% 
-% arc_places = ["p1", "p1", "p1", "p2", "p3", "p4"];
-% arc_trans  = ["t1", "t2", "t3", "t1", "t2", "t3"];
-% arc_type =   ["in", "in", "in", "out","out","out"];
-% arc_weights= [1   , 1   , 1   , 1    , 1   ,1   ];
-% 
-% reward_names = ["p1", "t1"];
-% reward_types = ["place", "transition"];
-% reward_values= [5, 3];
-% 
-% small_hybrid.add_places(places,tokens);
-% small_hybrid.add_transitions(transitions, transition_types, transition_rates);
-% small_hybrid.add_arcs(arc_places,arc_trans,arc_type,arc_weights);
-% small_hybrid.set_reward_functions(reward_names, reward_values, reward_types);
+small_hybrid = GSPNR();
+places = ["p1", "p2", "p3", "p4"];
+tokens = [2   , 0   , 0   , 0   ];
 
-% [MDP, markings, states, types] = small_hybrid.ConverttoMDP()
+transitions =       ["t1", "t2", "t3"];
+transition_types =  ["imm","imm","exp"];
+transition_rates =  [0,    0,    1   ];
+
+arc_places = ["p1", "p1", "p1", "p2", "p3", "p4"];
+arc_trans  = ["t1", "t2", "t3", "t1", "t2", "t3"];
+arc_type =   ["in", "in", "in", "out","out","out"];
+arc_weights= [1   , 1   , 1   , 1    , 1   ,1   ];
+
+reward_names = ["p1", "t1"];
+reward_types = ["place", "transition"];
+reward_values= [5, 3];
+
+small_hybrid.add_places(places,tokens);
+small_hybrid.add_transitions(transitions, transition_types, transition_rates);
+small_hybrid.add_arcs(arc_places,arc_trans,arc_type,arc_weights);
+small_hybrid.set_reward_functions(reward_names, reward_values, reward_types);
+
+[MDP, markings, states, types] = small_hybrid.toMDP()
+validity = MDP.check_validity()
+
+MDP.actions_enabled("S1")
+
+% test_norm = GSPNR();
+% places = ["p1", "p2", "p3", "p4", "p5"];
+% tokens = [2   , 0   , 0   , 0   , 0];
 % 
-test_norm = GSPNR();
-places = ["p1", "p2", "p3", "p4", "p5"];
-tokens = [1   , 0   , 0   , 0   , 0];
+% transitions =    ["t1", "t2", "t3", "t4", "t5", "t6"];
+% transition_types=["exp","exp","exp","exp","exp","exp"];
+% transition_rates=[1,    1,    2,    1.5,   3   , 1.5];
+% 
+% arc_places = ["p1","p1","p1","p2" , "p3","p4" ,"p4" , "p4", "p4", "p5", "p4", "p5"];
+% arc_trans  = ["t1","t2","t3","t1" , "t2","t3" ,"t4" , "t5", "t5", "t4", "t6", "t6"];
+% arc_type   = ["in","in","in","out","out","out","in" ,"in" ,"out","out", "in", "out"];
+% arc_weights= [1   ,1   ,1   ,1    ,1    ,1    ,1    ,1    ,1    ,1    , 1   , 1];
+% test_norm.add_places(places,tokens);
+% test_norm.add_transitions(transitions,transition_types,transition_rates);
+% test_norm.add_arcs(arc_places,arc_trans,arc_type,arc_weights);
+% 
+% [MDP, markings, states, types] = test_norm.toMDP()
+% 
+% validity = MDP.check_validity()
 
-transitions =    ["t1", "t2", "t3", "t4", "t5", "t6"];
-transition_types=["exp","exp","exp","exp","exp","exp"];
-transition_rates=[1,    1,    2,    1.5,   3   , 1.5];
-
-arc_places = ["p1","p1","p1","p2" , "p3","p4" ,"p4" , "p4", "p4", "p5", "p4", "p5"];
-arc_trans  = ["t1","t2","t3","t1" , "t2","t3" ,"t4" , "t5", "t5", "t4", "t6", "t6"];
-arc_type   = ["in","in","in","out","out","out","in" ,"in" ,"out","out", "in", "out"];
-arc_weights= [1   ,1   ,1   ,1    ,1    ,1    ,1    ,1    ,1    ,1    , 1   , 1];
-test_norm.add_places(places,tokens);
-test_norm.add_transitions(transitions,transition_types,transition_rates);
-test_norm.add_arcs(arc_places,arc_trans,arc_type,arc_weights);
-
-[MDP, markings, states, types] = test_norm.ConverttoMDP()
+[full_transition, full_rewards] = MDP.get_full_matrices();
--- bin/test_GSPN.m
+++ bin/test_GSPN.m
@@ -2,11 +2,11 @@ clear
 clc
 
 places = ["p1", "p2", "p3", "p4"];
-tokens = [0, 1, 1, 0];
+tokens = [1, 0, 0, 0];
 
 transitions = ["t1", "t2", "t3"];
 types = ["imm", "exp", "imm"];
-rates = [1, 1, 1];
+rates = [0, 1, 0];
 
 %transitions_2 = ["t4"];
 %types_2 = ["imm"];
@@ -23,28 +23,11 @@ reward_types = ["place", "transition"];
 test = GSPNR();
 test.add_places(places, tokens)
 test.add_transitions(transitions, types, rates)
-test.places;
-test.initial_marking;
-test.transitions;
 test.add_arcs(arc_places, arc_transitions, arc_type, arc_weight)
 test.set_reward_functions(reward_names, reward_values, reward_types)
 
+[MDP, markings, states, types] = test.toMDP()
 
-%test.add_transitions(transitions_2, types_2)
-%test.transitions;
-[imm_transitions, exp_transitions] = test.enabled_transitions()
-test
+[validity, cumulative_prob]  = MDP.check_validity()
 
-test.fire_transition("t2")
-
-test
-
-[imm_transitions, exp_transitions] = test.enabled_transitions()
-
-test.fire_transition(imm_transitions)
-
-test
-
-test.fire_transition("t1")
-
-test
+MDP.actions_enabled("S1")
