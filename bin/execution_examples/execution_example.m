ece8a56a7dc4bbd94002b9afe42ae6a9af4e558f
amdpaula <antonio.amdpaula@gmail.com>
2021-03-29 12:22:36 +0100

    Added utility function that creates preset YAML file for user to fill out, according to the GSPN to be executed

--- bin/execution_examples/execution_example.m
+++ bin/execution_examples/execution_example.m
@@ -7,11 +7,13 @@ addpath('/home/antonio/Repos/multi-robot-gspnr-toolbox/bin/execution_examples/')
 
 PNPRO_path = 'execution.PNPRO';
 pnml_file = 'execution_example.xml';
+yaml_filepath = 'test.yaml';
 yaml_file = 'example.yaml';
 
-%gspn = ImportfromPIPE(pnml_file);
-[nGSPN, gspn_list] = ImportfromGreatSPN(PNPRO_path);
-%PrepareROSExecution(gspn, yaml_file);
+gspn = ImportfromPIPE(pnml_file);
+PrepareYAML(gspn, yaml_filepath);
+%[nGSPN, gspn_list] = ImportfromGreatSPN(PNPRO_path);
+%PrepareROSExecution(gspn_list{1}, yaml_file);
 
 
               
---
+++ src/utils/PrepareYAML.m
@@ -0,0 +1,72 @@
+function PrepareYAML(gspn, yaml_filepath)
+%PREPAREYAML Summary of this function goes here
+%   Detailed explanation goes here
+    robot_type_struct = struct();
+
+    [exp_trans, exp_trans_indices] = gspn.get_exponential_transitions();
+    
+    nEXPTrans = size(exp_trans, 2);
+    
+    action_places = [];
+    
+    for t_index = 1:nEXPTrans
+        exp_trans_index = exp_trans_indices(t_index);
+        input_places = gspn.input_arcs(:,exp_trans_index);
+        input_place_indices = find(input_places);
+        action_places = cat(2, action_places, input_place_indices');
+    end
+    action_places = unique(action_places);
+    
+    nActionPlaces = size(action_places, 2);
+    
+    nRobots = input("Input the amount of different robot types\n");
+    robot_types_list = [string.empty];
+    
+    for r_index = 1:nRobots
+        robot_types_list(r_index) = input("\nName of robot type - ", 's');
+    end
+    
+    for p_index = 1:nActionPlaces
+        place_index = action_places(p_index);
+        place_name = gspn.places(place_index);
+        prompt_string = "\nTo which robot type does "+place_name+" pertain? ";
+        robot_type = input(prompt_string, 's');
+        
+        if isempty(find(robot_types_list == robot_type))
+            error('Please input a robot type previously declared');
+        end
+        
+        robot_type_struct.(robot_type).(place_name).action_name = '';
+        robot_type_struct.(robot_type).(place_name).message_type = '';
+        robot_type_struct.(robot_type).(place_name).message_fields = '';        
+    end
+    
+    more = "NOT_DONE";
+    
+    while (more ~= "DONE")
+        more = input("Are there any places not previously mentioned that are action places?\n If not input 'DONE', if so, input the place name\n",'s');
+        
+        if more == "DONE"
+            break
+        end
+        
+        if isempty(find(gspn.places == more))
+            error('Please input a valid place name');
+        end
+        prompt_string = "\nTo which robot type does "+more+" pertain? ";
+        robot_type = input(prompt_string, 's');
+        
+        if isempty(find(robot_types_list == robot_type))
+            error('Please input a robot type previously declared');
+        end
+        
+        robot_type_struct.(robot_type).(more).action_name = '';
+        robot_type_struct.(robot_type).(more).message_type = '';
+        robot_type_struct.(robot_type).(more).message_fields = '';
+        
+    end
+        
+    WriteYaml(yaml_filepath, robot_type_struct);
+
+end
+
