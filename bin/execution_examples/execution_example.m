045b9ba13e9cd13a4adc417df3f8e0db43682200
amdpaula <antonio.amdpaula@gmail.com>
2021-03-30 16:29:49 +0100

    Corrections to test files

--- bin/execution_examples/execution_example.m
+++ bin/execution_examples/execution_example.m
@@ -7,13 +7,16 @@ addpath('/home/antonio/Repos/multi-robot-gspnr-toolbox/bin/execution_examples/')
 
 PNPRO_path = 'execution.PNPRO';
 pnml_file = 'execution_example.xml';
-yaml_filepath = 'test.yaml';
 yaml_file = 'example.yaml';
+yaml_filepath = 'test.yaml';
 
 gspn = ImportfromPIPE(pnml_file);
 PrepareYAML(gspn, yaml_filepath);
 %[nGSPN, gspn_list] = ImportfromGreatSPN(PNPRO_path);
-%PrepareROSExecution(gspn_list{1}, yaml_file);
+action_place_struct = PrepareROSExecution(gspn, yaml_file);
+
+%ROSExecutionManager(gspn, [], [], action_place_struct);
+
 
 
               
