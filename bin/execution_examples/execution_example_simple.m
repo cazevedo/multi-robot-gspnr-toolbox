32dc913baf731cf4a881914fc01576bb299565eb
amdpaula <antonio.amdpaula@gmail.com>
2021-04-05 18:18:51 +0100

    Changed example to test new executable GSPNR class

--- bin/execution_examples/execution_example_simple.m
+++ bin/execution_examples/execution_example_simple.m
@@ -1,20 +1,18 @@
 clc
 clear
 
-addpath('/home/antonio/Repos/multi-robot-gspnr-toolbox/src/models/');
-addpath('/home/antonio/Repos/multi-robot-gspnr-toolbox/src/utils/');
-addpath('/home/antonio/Repos/multi-robot-gspnr-toolbox/bin/execution_examples/config/')
-
 PNPRO_path = 'execution.PNPRO';
 pnml_file = 'execution_example.xml';
 yaml_file = 'example.yaml';
 yaml_filepath = 'test.yaml';
 
 gspn = ImportfromPIPE(pnml_file);
-PrepareYAML(gspn, yaml_filepath);
+%PrepareYAML(gspn, yaml_filepath);
 %[nGSPN, gspn_list] = ImportfromGreatSPN(PNPRO_path);
 action_place_struct = PrepareROSExecution(gspn, yaml_file);
 
+exec = ExecutableGSPNR();
+exec.import_nonExecutable(gspn, action_place_struct);
 %ROSExecutionManager(gspn, [], [], action_place_struct);
 
 
