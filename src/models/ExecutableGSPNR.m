d73819403b11ee911c3dd1897f653cea3ac75a64
amdpaula <antonio.amdpaula@gmail.com>
2021-04-06 12:12:10 +0100

    Added function that checks if there are any transitions that introduce ambiguity in the robot/token mapping and sets the appropriate property as true

--- src/models/ExecutableGSPNR.m
+++ src/models/ExecutableGSPNR.m
@@ -5,6 +5,7 @@ classdef ExecutableGSPNR < GSPNR
     properties (SetAccess = private)
         place_actions = struct();
         policy;
+        ambiguity = false;
     end
     
     methods
@@ -12,20 +13,20 @@ classdef ExecutableGSPNR < GSPNR
             execGSPNR = execGSPNR@GSPNR();
         end
         
-        function import_nonExecutable(execGSPNR, GSPNR, action_map)
+        function import_nonExecutable(obj, GSPNR, action_map)
             %Copy properties of non-executable instance;
             copy_gspn = copy(GSPNR);
-            execGSPNR.places = copy_gspn.places;
-            execGSPNR.transitions = copy_gspn.transitions;
-            execGSPNR.type_transitions = copy_gspn.type_transitions;
-            execGSPNR.rate_transitions = copy_gspn.rate_transitions;
-            execGSPNR.input_arcs = copy_gspn.input_arcs;
-            execGSPNR.output_arcs = copy_gspn.output_arcs;
-            execGSPNR.arcs = copy_gspn.arcs;
-            execGSPNR.initial_marking = copy_gspn.initial_marking;
-            execGSPNR.current_marking = copy_gspn.current_marking;
-            execGSPNR.place_rewards = copy_gspn.place_rewards;
-            execGSPNR.transition_rewards = copy_gspn.transition_rewards;
+            obj.places = copy_gspn.places;
+            obj.transitions = copy_gspn.transitions;
+            obj.type_transitions = copy_gspn.type_transitions;
+            obj.rate_transitions = copy_gspn.rate_transitions;
+            obj.input_arcs = copy_gspn.input_arcs;
+            obj.output_arcs = copy_gspn.output_arcs;
+            obj.arcs = copy_gspn.arcs;
+            obj.initial_marking = copy_gspn.initial_marking;
+            obj.current_marking = copy_gspn.current_marking;
+            obj.place_rewards = copy_gspn.place_rewards;
+            obj.transition_rewards = copy_gspn.transition_rewards;
             %Fill out the action properties;
             robot_types = string(fieldnames(action_map));
             nTypes = size(robot_types, 1);
@@ -36,14 +37,32 @@ classdef ExecutableGSPNR < GSPNR
                 for a_index = 1:nActionPlaces
                     place_name = action_places(a_index);
                     place_index = GSPNR.find_place_index(place_name);
-                    execGSPNR.place_actions(place_index).place_name = place_name;
-                    execGSPNR.place_actions(place_index).action_name = action_map.(type).(place_name).action_name;
-                    execGSPNR.place_actions(place_index).message = action_map.(type).(place_name).message;
+                    obj.place_actions(place_index).place_name = place_name;
+                    obj.place_actions(place_index).action_name = action_map.(type).(place_name).action_name;
+                    obj.place_actions(place_index).message = action_map.(type).(place_name).message;
                 end
             end     
         end
         
-        function set_policy(execGSPNR, markings, transition)
+        function check_robot_ambiguity(obj)
+            nTrans = size(obj.transitions, 2);
+            for t_index = 1:nTrans
+                [input_place_indices, col, val] = find(obj.input_arcs(:,t_index));
+                if sum(val)>1
+                    disp_string = "There is an input token/robot ambiguity involving transition - "+obj.transitions(t_index);
+                    disp(disp_string);
+                    ambiguity = true;
+                end
+                [row, output_place_indices, val] = find(obj.output_arcs(t_index, :));
+                if sum(val)>1
+                    disp_string = "There is an output token/robot ambiguity involving transition - "+obj.transitions(t_index);
+                    disp(disp_string);
+                    ambiguity = true;
+                end
+            end
+        end
+        
+        function set_policy(obj, markings, transition)
             
         end
         
