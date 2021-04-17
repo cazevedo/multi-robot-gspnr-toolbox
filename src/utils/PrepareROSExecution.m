4689e85bf62c5b17ea59cc065ffd335a4c75057f
amdpaula <antonio.amdpaula@gmail.com>
2021-04-17 17:54:21 +0100

    Changed to only use one robot type, and read fields to define action server names

--- src/utils/PrepareROSExecution.m
+++ src/utils/PrepareROSExecution.m
@@ -8,27 +8,24 @@ function [action_place_struct] = PrepareROSExecution(yaml_filepath)
     YamlStruct = ReadYaml(yaml_filepath);
     
     %Cell Array
-    robot_types = fieldnames(YamlStruct);
-    n_types = size(robot_types, 1);
-    
-    for t_index = 1:n_types
-        robot_type = robot_types{t_index};
-        action_places = fieldnames(YamlStruct.(robot_type));
-        nActionPlaces = size(action_places,1);
-        for a_index = 1:nActionPlaces
-            place_name = action_places{a_index};
-            action_name     = YamlStruct.(robot_type).(place_name).x0x2Daction_name;
-            message_type    = YamlStruct.(robot_type).(place_name).x0x2Dmessage_type;
-            msg = rosmessage(message_type);
-            message_fields  = fieldnames(YamlStruct.(robot_type).(place_name).x0x2Dmessage_fields);
-            nMsgFields = size(message_fields, 1);
-            for m_index = 1:nMsgFields
-                field_name = message_fields{m_index};
-                msg.(field_name) = YamlStruct.(robot_type).(place_name).x0x2Dmessage_fields.(field_name);
-                action_place_struct.(robot_type).(place_name).action_name = action_name;
-                action_place_struct.(robot_type).(place_name).message = msg;
-            end
+    action_places = fieldnames(YamlStruct);
+    nActionPlaces = size(action_places,1);
+    for a_index = 1:nActionPlaces
+        place_name = action_places{a_index};
+        server_name     = YamlStruct.(place_name).x0x2Daction_server_name;
+        action_name     = YamlStruct.(place_name).x0x2Daction_name;
+        package_name    = YamlStruct.(place_name).x0x2Dpackage_name;
+        message_fields  = fieldnames(YamlStruct.(place_name).x0x2Dmessage_fields);
+        nMsgFields = size(message_fields, 1);
+        msg = struct();
+        for m_index = 1:nMsgFields
+            field_name = message_fields{m_index};
+            msg.(field_name) = YamlStruct.(place_name).x0x2Dmessage_fields.(field_name);
         end
+        action_place_struct.(place_name).server_name = server_name;
+        action_place_struct.(place_name).action_name = action_name;
+        action_place_struct.(place_name).package_name = package_name;
+        action_place_struct.(place_name).message = msg;
     end
 end
 
