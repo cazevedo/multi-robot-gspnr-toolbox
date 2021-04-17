b5474e5330e8018a277c27565a47902f4ee2ca1a
amdpaula <antonio.amdpaula@gmail.com>
2021-04-17 17:50:55 +0100

    Added two helper functions for execution manager

---
+++ src/utils/CheckRobotsInvolved.m
@@ -0,0 +1,21 @@
+function robots_involved = CheckRobotsInvolved(exec, transition, RobotPlaces)
+%CHECKROBOTSINVOLVED Summary of this function goes here
+%   Detailed explanation goes here
+    robots_involved = [];
+    trans_index = exec.find_transition_index(transition);
+    [input_place_indices, col, val] = find(exec.input_arcs(:, trans_index));
+    nInputPlaces = size(input_place_indices, 1);
+    for pi_index = 1:nInputPlaces
+        input_place_index = input_place_indices(pi_index);
+        input_place_name = exec.places(input_place_index);
+        arc_weight = val(pi_index);
+        %Check if input place is a robot place
+        if ~isempty(find(exec.robot_places == input_place_name))
+            %If so, the number of robots involved is the arc weight
+            %Check which robots are in this place
+            robots_in_place = find(RobotPlaces == input_place_index);
+            robots_involved = cat(2, robots_involved, randsample(robots_in_place, arc_weight));
+        end
+    end
+end
+
---
+++ src/utils/UpdateRobotPlaces.m
@@ -0,0 +1,38 @@
+function new_robot_places = UpdateRobotPlaces(exec, transition, old_robot_places, robots_to_update)
+    %Save old robot places
+    new_robot_places = old_robot_places;
+    trans_index = exec.find_transition_index(transition);
+    [input_place_indices, col, input_val] = find(exec.input_arcs(:,trans_index));
+    nInputPlaces = size(input_place_indices, 1);
+    nInputRobots = 0;
+    for pi_index = 1:nInputPlaces
+        input_place_name = exec.places(input_place_indices(pi_index));
+        if ~isempty(exec.robot_places == input_place_name)
+            %Is a robot place, will remove a robot token
+            nInputRobots = nInputRobots + input_val(pi_index);
+        end
+    end
+    nRobotsInvolved = size(robots_to_update, 2);
+    
+    if (nInputRobots ~= nRobotsInvolved)
+        error("The number of robots affected by transition is not the same number of robots given as input");
+    end
+    
+    [row, output_place_indices, val] = find(exec.output_arcs(trans_index, :));
+    nOutputPlaces = size(output_place_indices, 2);
+    
+    for ri_index = 1:nRobotsInvolved
+        r_index = robots_to_update(ri_index);
+        old_place_index = old_robot_places(r_index);
+        if ~isempty(find(input_place_indices == old_place_index))
+            for o_index = 1:nOutputPlaces
+                output_place_name = exec.places(output_place_indices(o_index))
+                if ~isempty(find(exec.robot_places == output_place_name))
+                    new_robot_places(r_index) = output_place_indices(o_index);
+                    break
+                end
+            end
+        end
+    end
+    
+end
