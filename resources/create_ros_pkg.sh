55381a1639752c25980bbd86d01d4338e8a2903f
Carlos Azevedo <cguerraazevedo@tecnico.ulisboa.pt>
2021-04-21 18:18:42 +0100

    create ros package bash script

---
+++ resources/create_ros_pkg.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# check that the number of received arguments are correct
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters"
+    echo "Usage : bash create_robocup_pkg.sh name_of_my_awesome_pkg"
+    exit
+fi
+
+# assign first received argument to variable PKG_NAME
+PKG_NAME=${1}
+AUTHOR_NAME="multi-robot-matlab-gspnr"
+AUTHOR_EMAIL="multi-robot-matlab-gspnr"
+MAINTAINER_NAME="multi-robot-matlab-gspnr"
+MAINTAINER_EMAIL="multi-robot-matlab-gspnr"
+
+# setup file content
+CMAKELISTS_CONTENT="cmake_minimum_required(VERSION 3.0.2)\nproject(${PKG_NAME})\n\nfind_package(catkin REQUIRED COMPONENTS\n actionlib\n actionlib_msgs\n actionlib_tutorials\n rospy\n std_msgs\n)\n\ncatkin_python_setup()\n\ncatkin_package(\n    CATKIN_DEPENDS actionlib actionlib_msgs actionlib_tutorials rospy std_msgs\n)"
+PACKAGE_XML_CONTENT="<?xml version=\"1.0\"?>\n<package>\n  <name>${PKG_NAME}</name>\n  <version>1.0.0</version>\n  <description>\n    TODO!\n  </description>\n\n <maintainer email=\"antonio@todo.todo\">antonio</maintainer>\n\n <license>GPLv3</license>\n\n  \n  <buildtool_depend>catkin</buildtool_depend>\n\n  <build_depend>actionlib</build_depend>\n<build_depend>actionlib_msgs</build_depend>\n<build_depend>actionlib_tutorials</build_depend>\n<build_depend>rospy</build_depend>\n<build_depend>std_msgs</build_depend>\n\n<run_depend>actionlib</run_depend>\n<run_depend>actionlib_msgs</run_depend>\n<run_depend>actionlib_tutorials</run_depend>\n<run_depend>rospy</run_depend>\n<run_depend>std_msgs</run_depend>\n\n</package>"
+SETUP_PY_CONTENT="#!/usr/bin/env python\n\nfrom distutils.core import setup\nfrom catkin_pkg.python_setup import generate_distutils_setup\n\n# for your packages to be recognized by python\nd = generate_distutils_setup(\n  packages=['${PKG_NAME}'],\n  package_dir={'': 'src'}\n)\n\nsetup(**d)"
+
+# create folder structure
+# roscd
+mkdir $ROS_WORKSPACE/${PKG_NAME}
+# cd ${PKG_NAME}
+mkdir $ROS_WORKSPACE/${PKG_NAME}/src
+mkdir $ROS_WORKSPACE/${PKG_NAME}/launch
+
+# create file structure
+touch $ROS_WORKSPACE/${PKG_NAME}/CMakeLists.txt
+touch $ROS_WORKSPACE/${PKG_NAME}/package.xml
+touch $ROS_WORKSPACE/${PKG_NAME}/setup.py
+
+# fill files with information from parameter section
+echo -e $CMAKELISTS_CONTENT  > $ROS_WORKSPACE/${PKG_NAME}/CMakeLists.txt
+echo -e $PACKAGE_XML_CONTENT > $ROS_WORKSPACE/${PKG_NAME}/package.xml
+echo -e $SETUP_PY_CONTENT > $ROS_WORKSPACE/${PKG_NAME}/setup.py
+
+
+# catkin build --this
+# source ~/.bashrc
