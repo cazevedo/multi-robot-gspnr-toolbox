4f3ea3a71954b6fbfcaca35f59eca28663f693c0
rui-bettencourt <rui.bett.96@hotmail.com>
2021-05-21 09:13:27 +0100

    Harode update with test results

---
+++ res/scalability/reset_LD_LIBRARY_PATH.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+DEFAULT=/home/harode/Software/nuance/bin/linux/i86:/home/harode/tiago_ws/devel/lib:/opt/ros/melodic/lib:/opt/ros/melodic/lib/x86_64-linux-gnu:/home/harode/Software/nuance/bin/linux/i86:/home/harode/Software/nuance/bin/linux/i86:/home/harode/Software/nuance/bin/linux/i86
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$DEFAULT;
--- res/scalability/run_ExecutableGSPNRInitialize.m
+++ res/scalability/run_ExecutableGSPNRInitialize.m
@@ -2,7 +2,7 @@ function run_ExecutableGSPNRInitialize(nLocations)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
     cd ..;cd ..;cd ..;
-    
+    pwd
     p = genpath('multi-robot-gspnr-toolbox');
     addpath(p);
     cd 'multi-robot-gspnr-toolbox';
@@ -10,6 +10,7 @@ function run_ExecutableGSPNRInitialize(nLocations)
     cd 'logs/scalability/increasing_locations/';
     filename = "locations"+string(nLocations)+"_creation_test.mat";
     save(filename, 'executable', 'pre_execution_parameters', '-append');
+    pause();
     quit();
 end
 
